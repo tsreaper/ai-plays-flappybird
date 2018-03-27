9d719f38fa3aa1434b7b659eec138433509b7152
ruslan.lopez <ruslan.lopez@globant.com>
2018-03-27 06:10:09 -0600

    reduce cognitive complexity for method update game

--- js/game.js
+++ js/game.js
@@ -28,57 +28,66 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.updateGame = function () {
         if (!this.gameover) {
-            // Move pipes
-            for (var i = 0; i < Data.game.PIPE_NUM; i++) {
-                this.pipeX[i] -= Data.game.MOVE_SPEED;
-                if (this.pipeX[i] <= -Data.game.PIPE_WIDTH) {
-                    this.pipeX[i] = Data.animation.SCREEN_WIDTH;
-                    this.pipeUpper[i] = this._getNewPipeY();
-                }
-            }
-            if (this.pipeX[this.nextPipe] < Data.game.BIRD_INIT_X - Data.game.PIPE_WIDTH - Data.game.BIRD_RADIUS) {
-                this.currentScore++;
-            }
-            // Find next pipe
-            this.nextPipe = Data.game.PIPE_NUM;
-            for (var i = 0; i < Data.game.PIPE_NUM; i++) {
-                if (this.pipeX[i] >= Data.game.BIRD_INIT_X - Data.game.PIPE_WIDTH - Data.game.BIRD_RADIUS && this.pipeX[i] < this.pipeX[this.nextPipe]) {
-                    this.nextPipe = i;
-                }
-            }
+            this.movePipes();
+            this.findNextPipe();
         }
-        // Move birds
-        for (var i = 0; i < Data.generation.BIRD_NUM; i++) {
-            this.generation.birds[i].fly(this.pipeX[this.nextPipe] - Data.game.BIRD_INIT_X, this.pipeUpper[this.nextPipe]);
-            if (this.generation.birds[i].alive) {
-                this.generation.birds[i].score = this.currentScore;
-                // Bird hit the land
-                if (this.generation.birds[i].y + Data.game.BIRD_RADIUS >= Data.game.LAND_Y) {
-                    this.generation.birds[i].alive = false;
-                }
-                else if (this.generation.birds[i].y <= -Data.game.BIRD_RADIUS) {
-                    this.generation.birds[i].alive = false;
-                }
-                else if (this.pipeX[this.nextPipe] - Data.game.BIRD_INIT_X <= Data.game.BIRD_RADIUS) {
-                    if (this.generation.birds[i].y - Data.game.BIRD_RADIUS <= this.pipeUpper[this.nextPipe] || this.generation.birds[i].y + Data.game.BIRD_RADIUS >= this.pipeUpper[this.nextPipe] + Data.game.SPACE_HEIGHT) {
-                        this.generation.birds[i].alive = false;
-                    }
-                }
-                if (!this.generation.birds[i].alive) {
-                    this._aliveNum--;
-                }
-            }
-            else if (!this.gameover) {
-                this.generation.birds[i].x -= Data.game.MOVE_SPEED;
-            }
-            // Prevent the bird from falling below the lower edge of the canvas
-            if (this.generation.birds[i].y + Data.game.BIRD_RADIUS >= Data.game.LAND_Y) {
-                this.generation.birds[i].y = Data.game.LAND_Y - Data.game.BIRD_RADIUS;
-            }
-        }
+        this.moveBirds();
         this.checkForGameOver();
         dashboard.updateDashboard();
         animation.updateAnimation();
+    };
+    Game.prototype.movePipes = function () {
+        for (var pipeIdx = 0; pipeIdx < Data.game.PIPE_NUM; pipeIdx++) {
+            this.pipeX[pipeIdx] -= Data.game.MOVE_SPEED;
+            if (this.pipeX[pipeIdx] <= -Data.game.PIPE_WIDTH) {
+                this.pipeX[pipeIdx] = Data.animation.SCREEN_WIDTH;
+                this.pipeUpper[pipeIdx] = this._getNewPipeY();
+            }
+        }
+        if (this.pipeX[this.nextPipe] < Data.game.BIRD_INIT_X - Data.game.PIPE_WIDTH - Data.game.BIRD_RADIUS) {
+            this.currentScore++;
+        }
+    };
+    Game.prototype.findNextPipe = function () {
+        this.nextPipe = Data.game.PIPE_NUM;
+        for (var i = 0; i < Data.game.PIPE_NUM; i++) {
+            if (this.pipeX[i] >= Data.game.BIRD_INIT_X - Data.game.PIPE_WIDTH - Data.game.BIRD_RADIUS && this.pipeX[i] < this.pipeX[this.nextPipe]) {
+                this.nextPipe = i;
+            }
+        }
+    };
+    Game.prototype.moveBirds = function () {
+        for (var birdIdx = 0; birdIdx < Data.generation.BIRD_NUM; birdIdx++) {
+            this.generation.birds[birdIdx].fly(this.pipeX[this.nextPipe] - Data.game.BIRD_INIT_X, this.pipeUpper[this.nextPipe]);
+            if (this.generation.birds[birdIdx].alive) {
+                this.moveAliveBird(birdIdx);
+            }
+            else if (!this.gameover) {
+                this.generation.birds[birdIdx].x -= Data.game.MOVE_SPEED;
+            }
+            // Prevent the bird from falling below the lower edge of the canvas
+            if (this.generation.birds[birdIdx].y + Data.game.BIRD_RADIUS >= Data.game.LAND_Y) {
+                this.generation.birds[birdIdx].y = Data.game.LAND_Y - Data.game.BIRD_RADIUS;
+            }
+        }
+    };
+    Game.prototype.moveAliveBird = function (birdIdx) {
+        this.generation.birds[birdIdx].score = this.currentScore;
+        // Bird hit the land
+        if (this.generation.birds[birdIdx].y + Data.game.BIRD_RADIUS >= Data.game.LAND_Y) {
+            this.generation.birds[birdIdx].alive = false;
+        }
+        else if (this.generation.birds[birdIdx].y <= -Data.game.BIRD_RADIUS) {
+            this.generation.birds[birdIdx].alive = false;
+        }
+        else if (this.pipeX[this.nextPipe] - Data.game.BIRD_INIT_X <= Data.game.BIRD_RADIUS) {
+            if (this.generation.birds[birdIdx].y - Data.game.BIRD_RADIUS <= this.pipeUpper[this.nextPipe] || this.generation.birds[birdIdx].y + Data.game.BIRD_RADIUS >= this.pipeUpper[this.nextPipe] + Data.game.SPACE_HEIGHT) {
+                this.generation.birds[birdIdx].alive = false;
+            }
+        }
+        if (!this.generation.birds[birdIdx].alive) {
+            this._aliveNum--;
+        }
     };
     Game.prototype.checkForGameOver = function () {
         if (!this._aliveNum && !this.gameover) {
